bbe115287debaacaee2304593c0af9a69003407e
pktliga <69900785+pktliga@users.noreply.github.com>
2021-05-16 17:18:22 +0200

    Update los.js

--- jsscript/los.js
+++ jsscript/los.js
@@ -1,155 +1,135 @@
-var players = ["MARIUSZ.N",
-      "MAREK.M",
-      "SŁAWEK.M",
-      "STASIU.M",
-      "ARTUR.M",
-      "MAREK.O",
-      "ADAM.C",
-      "RYSIU.N",
-      "JUREK.Ł",
-      "ZBYSZEK.A",
-      "HUBERT.K",
-      "KRZYSZTOF.W",
-      "JACEK.D",
-      "RYSIU.CH",
-      "HUBERT.Z",
-      "GENIU.Z",
-      "KUBA.C",
-      "ROBERT",
-      "KAROL",
-      "ANDRZEJ.Z"
-    ];
-      players.sort();
-      var a = 1;
-      var b = 18;
-      for (var i = 0; i < players.length; i++) {
-        checkBoxDisplay.innerHTML+='<input id="ch'+a+'" type="checkbox" name="playername" value="'+players[i]+'">'+'<label for="ch'+a+'">'+players[i]+'</label>'+"<br>";
-        a++;
+  var players =["MARIUSZ.N",
+                "MAREK.M",
+                "SŁAWEK.M",
+                "STASIU.M",
+                "ARTUR.M",
+                "MAREK.O",
+                "ADAM.C",
+                "RYSIU.N",
+                "JUREK.Ł",
+                "ZBYSZEK.A",
+                "HUBERT.K",
+                "KRZYSZTOF.W",
+                "JACEK.D",
+                "RYSIU.CH",
+                "HUBERT.Z",
+                "GENIU.Z",
+                "KUBA.C",
+                "ROBERT.G",
+                "KAROL",
+                "ANDRZEJ.Z"
+              ];
+
+  players.sort();
+  var a = 0;
+  for (var i = 0; i < players.length; i++) {
+       checkBoxDisplay.innerHTML+='<input id="ch'+a+'" type="checkbox" class="checks1" name="playername" value="'+players[i]+'">'+'<label for="ch'+a+'">'+players[i]+'</label>'+"<br>";
+       a++;
+  }
+  var firstplayerpick = [];
+  var secplayerpick = [];
+
+  function getCheckedValues(){
+      var checkboxes = document.getElementsByName("playername");
+      for(var i = 0; i < checkboxes.length; i++)
+      {
+          if(checkboxes[i].checked)
+              //firstplayerpick[i]=checkboxes[i].value;
+              //console.log(checkboxes[i].value)
+              firstplayerpick.push(checkboxes[i].value)
       }
-    function getCheckedCheckboxesFor(checkboxName) {
-          var checkboxes = document.querySelectorAll('input[name="' + checkboxName + '"]:checked'), playerpick = [];
-          Array.prototype.forEach.call(checkboxes, function(el) {-
-              playerpick.push(el.value);
-          });
-          return playerpick;
+
+        console.log(firstplayerpick);
+  }
+
+  button1.onclick = function(){
+      getCheckedValues();
+      if(firstplayerpick.length==0){
+        alert("Wybierz zawodników!!");
+      }else{
+        const valuesToRemove = firstplayerpick;
+        const filteredItems = players.filter(item => !valuesToRemove.includes(item));
+
+        console.log(filteredItems);
+
+        checkBoxDisplay.style.display="none";
+        button1.style.display="none";
+        button2.style.display="block";
+        result1.style.display="block";
+
+        for (var i = 0; i < filteredItems.length; i++) {
+            result1.innerHTML+='<input id="ch'+a+'" type="checkbox" class="checks2" name="playername2" value="'+filteredItems[i]+'">'+'<label for="ch'+a+'">'+filteredItems[i]+'</label>'+"<br>";
+            a++;
         }
-      button1.onclick = function(){
+      }
+      window.scrollTo(0, 0);
+  }
 
-        var playerpick = getCheckedCheckboxesFor('playername');
+  function getCheckedValues2(){
+      var checkboxes = document.getElementsByName("playername2");
+      for(var i = 0; i < checkboxes.length; i++)
+      {
+          if(checkboxes[i].checked)
+              secplayerpick.push(checkboxes[i].value)
+      }
+  }
 
-
-        if(playerpick.length==0){
-
-          alert("Nie wybrano żadnego zawodnika");
-        }else if( playerpick.length<2){
-          alert("Trzeba wybrać co najmniej  dwóch zawodników")
+  button2.onclick = function(){
+    getCheckedValues2();
+    console.log(secplayerpick);
+        if(secplayerpick.length<firstplayerpick.length){
+          alert("Wybrano za mało zawodników");
         }else{
+          cntvis.style.display="none";
 
-
-          const valuesToRemove = playerpick;
-          const filteredItems = players.filter(item => !valuesToRemove.includes(item));
-
-          console.log(filteredItems);
-
-          checkBoxDisplay.style.display="none";
-          result1.style.display="block";
-          button1.style.display="none";
-          button2.style.display="block";
-
-          for (var i = 0; i < filteredItems.length; i++) {
-            result1.innerHTML+='<input id="ch'+b+'" type="checkbox" name="playername2" value="'+filteredItems[i]+'">'+'<label for="ch'+b+'">'+filteredItems[i]+'</label>'+"<br>";
-            b++;
-          }
-          console.log(playerpick);
-          window.scrollTo(0, 0);
-        }
-      }
-        function getCheckedSecondPick(checkboxName2){
-          var checkboxes = document.querySelectorAll('input[name="' + checkboxName2 + '"]:checked'), secplayerpick = [];
-          Array.prototype.forEach.call(checkboxes, function(el) {
-            secplayerpick.push(el.value);
-          });
-          console.log(secplayerpick);
-          var playerpick = getCheckedCheckboxesFor('playername');
-          var counter = 1;
-          var pl = playerpick.length;
-          var sc = secplayerpick.length;
           function* shuffle(array1) {
               var i = array1.length;
               while (i--) {
                   yield array1.splice(Math.floor(Math.random() * (i+1)), 1)[0];
               }
           }
-          if(secplayerpick.length==0){
-            alert("Nie wybrano żadnego zawodnika!!");
-          }else if(secplayerpick.length<playerpick.length) alert("Wybrano za mało zawodników");
-          //else if(secplayerpick.length>playerpick.length+1) alert("Wbrano za dużo zawodników");
-          else{
-            cntvis.style.display="none";
-            var random1 = shuffle(secplayerpick);
-            var random2 = shuffle(playerpick);
-            var table = document.getElementById("myTable");
+          var random1 = shuffle(firstplayerpick);
+          var random2 = shuffle(secplayerpick);
+          var table = document.getElementById("myTable");
+          var sum = firstplayerpick.length+secplayerpick.length;
 
-            //for (var i = 0; i < sc; i++) {
-            var i=0;
+          for (var i = 0; i < sum/2; i++) {
 
 
-            function tloop(){
-              var both = pl+sc;
+
             setTimeout(function(){
 
+
               var row = table.insertRow(0);
-              var cell2 = row.insertCell(0);
-              var cell3 = row.insertCell(1);
               var cell1 = row.insertCell(0);
+              var cell2 = row.insertCell(1);
+              if(firstplayerpick.length==0){
+                cell1.innerHTML = random2.next().value;
+                if(secplayerpick.length==0){
 
-            //  if(playerpick.length==0){
-              //  cell2.innerHTML="PARA NR.1";
-            //  }else
+                  cell2.innerHTML = "PARA NR.1";
+                }else{
 
-
-            if(playerpick.length==0){
-              if(secplayerpick.length==1){
-                cell2.innerHTML = random1.next().value;
-                cell3.innerHTML = "PARA NR.1";
+                  cell2.innerHTML = random2.next().value;
+                }
               }else{
-                cell2.innerHTML = random1.next().value;
-                cell3.innerHTML = random1.next().value;
+
+                cell1.innerHTML = random1.next().value;
+                cell2.innerHTML = random2.next().value;
               }
-            }else{
-              cell3.innerHTML = random1.next().value;
-              cell2.innerHTML = random2.next().value;
-            }
 
-
-              cell1.innerHTML = counter;
-              counter++;
-              i++;
-
-
-              if(i<both/2){
-                tloop();
-              }else reloadbutton.style.display="block";
             },1000);
-            //}
           }
-
-
-            tloop();
-
-
-
-
-
-
+            reloadbutton.style.display="block";
 
             reloadbutton.onclick = function(){
               location.reload();
             }
             window.scrollTo(0,0);
-            console.log(secplayerpick);
-            console.log(playerpick);
-          }
+
 
 
         }
+
+
+  }
